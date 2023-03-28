ebbdc19f74abe9f21ac85fe721c2236a9a396cc7
Manish KC <manish.kc@renegadeinsurance.com>
2023-03-28 19:39:58 +0545

    feat: clean up the game card component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,11 @@ const GameGrid = ({ gameQuery }: Props) => {
 
   const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
-    <SimpleGrid columns={{ sm: 1, md: 2, lg: 4 }} padding="10px" spacing={3}>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
       {isLoading
         ? skeleton.map((skeleton) => (
             <GameCardContainer key={skeleton}>
