3bc3d1cdc7a1805360f4bb2f9d1af1a30db2cfee
Manish KC <kcmanish104@gmail.com>
2023-11-17 21:11:02 +0100

    create game attribute section

--- src/interfaces/Game.ts
+++ src/interfaces/Game.ts
@@ -1,9 +1,13 @@
+import { Genre } from "../hooks/useGenre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: string;
   name: String;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
